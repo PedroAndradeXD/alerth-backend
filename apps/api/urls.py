385a8cc7c2c15cb84ac6bbf7b025c1d5c84f5503
Pedro Andrade <pedrohlandradexd@gmail.com>
2024-10-10 22:07:21 -0300

    Criando os endpoints de autenticação

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.http import HttpResponse
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 def returnResponde(request):
@@ -10,4 +11,9 @@ def returnResponde(request):
 # Definindo as URLs
 urlpatterns = [
     path('endpoint/', returnResponde, name='apps'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login', views.login),
+    path('signup', views.signup),
+    path('test_token', views.test_token),
 ]
